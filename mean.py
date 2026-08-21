a30fc50d4359367ca97bec117121bd0a5d152abe
Harsha <harshayt8@gmail.com>
2026-08-21 12:09:06 +0530

    adding files

--- mean.py
+++ mean.py
@@ -1,4 +1,7 @@
 '''
 Statistics is the process of collecting, organizing, analyzing, interpreting, and presenting data.
+Types of Statistics 
+1.) Descriptive Statistics (Summarizing Data)
+2.) Inferential Statistics
 
 '''
