c8dcc2b38ed4560da93a4fbcc47fe3253478123d
Harsha <harshayt8@gmail.com>
2026-08-21 12:13:17 +0530

    discriptive statistics

--- mean.py
+++ mean.py
@@ -4,4 +4,36 @@ Types of Statistics
 1.) Descriptive Statistics (Summarizing Data)
 2.) Inferential Statistics
 
+1.) Descriptive Statistics (Summarizing Data): 
+This type summarizes and organizes data using
+measures like mean, median, and mode. It includes measures of
+dispersion like range, variance, and standard deviation. Descriptive
+statistics is used to describe the dataset and does not make predictions
+
+Must Know
+Mean
+Median
+Mode
+Minimum/Maximum
+Range
+Variance
+Standard deviation
+Percentiles
+Quartiles
+IQR
+Outliers
+Distribution
+Normal distribution
+Skewness
+Correlation
+Covariance
+Probability basics
+Sampling
+Confidence intervals
+Hypothesis testing
+Null & alternative hypothesis
+p-value
+Statistical significance
+Type I & Type II errors
+
 '''
